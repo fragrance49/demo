127085914cd05e78f4dbe28807f6632bff0da072
Full Stack Developer <74105421+fragrance49@users.noreply.github.com>
2025-07-09 07:56:59 +0800

    init demo

---
+++ README.md
@@ -0,0 +1,36 @@
+# 🛠️ DEX Platform - Local Setup Guide
+
+Welcome! This document will guide you through setting up the DEX platform locally for development and testing.
+
+---
+
+## 📦 Prerequisites
+
+Ensure the following are installed on your system:
+
+- [Node.js](https://nodejs.org/) v18+  
+- [Yarn](https://classic.yarnpkg.com/lang/en/docs/install/) or npm
+- [Git](https://git-scm.com/)  
+
+---
+
+## 🚀 Quick Start
+
+### 1. Clone the Repository
+
+```bash
+git clone https://acebrian64-admin@bitbucket.org/acebrian604/fm_dex.git
+cd dex_demo
+```
+
+### 2. Install Dependencies
+
+```bash
+npm install --force
+```
+
+### 3. Start the project
+
+```bash
+npm start
+```
---
+++ server/config/constant.js
@@ -0,0 +1,20 @@
+
+const PROJECT_ID = "EOLFE934KEKFEASSDWE"; // You can change with your project id
+
+const EthMainnet = `https://mainnet.infura.io/v3/${PROJECT_ID}`
+const PolygonMainnet = `https://polygon-mainnet.infura.io/v3/${PROJECT_ID}`
+const BscMainnet = "https://bsc-dataseed.binance.org"
+const ArbitrumMainnet = "https://arb1.arbitrum.io/rpc"
+const Avalanche = "https://api.avax.network/ext/bc/C/rpc"
+const Fantom = "https://rpcapi.fantom.network"
+const Harmony = "https://api.harmony.one"
+const Heco = "https://http-mainnet.hecochain.com"
+const Klay = "https://node-api.klaytn.com"
+const Matic = "https://rpc-mainnet.maticvigil.com"
+const Moonbeam = "https://rpc.testnet.moonbeam.network"
+const Optimism = "https://mainnet.optimism.io"
+const Palm = `https://palm-mainnet.infura.io/v3/${PROJECT_ID}`
+const Ronin = "https://api.roninchain.com"
+const xDai = "https://rpc.xdaichain.com"
+
+module.exports =  { EthMainnet, PolygonMainnet, BscMainnet, ArbitrumMainnet, Avalanche, Fantom, Harmony, Heco, Klay, Matic, Moonbeam, Optimism, Palm, Ronin, xDai };
